863d778f131e11611833788f8b5d33db7004fa48
Jessica Bull <117891001+j-bull2003@users.noreply.github.com>
2023-02-21 17:33:43 +0000

    Update api.jsx

--- client/src/Config/api.jsx
+++ client/src/Config/api.jsx
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
     apiUrl = `http://localhost:3000`
 }
 else {
-    apiUrl = `https://dev-challenge-production.up.railway.app`
+    apiUrl = `https://right-sofa-production.up.railway.app`
 }
 
 export default apiUrl
